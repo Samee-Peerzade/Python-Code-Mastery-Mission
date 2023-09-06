610a9bca82a4a7bddc696345b75bef40f642f560
Samee Peerzade <109617585+Samee-Peerzade@users.noreply.github.com>
2023-09-06 14:21:25 -0700

    Add files via upload

---
+++ Easy/Count_Vowels_in_String.py
@@ -0,0 +1,62 @@
+""" 
+2586. Count the Number of Vowel Strings in Range
+Easy
+268
+18
+Companies
+You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+Example 1:
+
+Input: words = ["are","amy","u"], left = 0, right = 2
+Output: 2
+Explanation: 
+- "are" is a vowel string because it starts with 'a' and ends with 'e'.
+- "amy" is not a vowel string because it does not end with a vowel.
+- "u" is a vowel string because it starts with 'u' and ends with 'u'.
+The number of vowel strings in the mentioned range is 2.
+Example 2:
+
+Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+Output: 3
+Explanation: 
+- "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+- "mu" is not a vowel string because it does not start with a vowel.
+- "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+- "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+The number of vowel strings in the mentioned range is 3.
+
+
+"""
+
+
+def vowelsString(words,left,right):
+    d=["a","e","i","o","u"]
+    count=0
+    
+    for i in range(left,right+1):
+        if(words[i][0] in d and words[i][-1] in d):
+            count=count+1
+            
+    return count
+    
+    
+    
+words=["are","amy","u"]
+left=0
+right=2
+print(vowelsString(words,left,right))
+
+
+#ouput=2 "because "are" and "u" are 2 vowels present in words.
+
+
+
+
+
+
+
+
